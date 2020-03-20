d51a55412b2aca0599dcb3b2e7dc6ddb03c3a571
jay-sun <j@j.j>
2020-03-20 01:19:10 -0700

    Update: 3/20/20 1:19 AM

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1584605706",
+        "cacheTag": "1584692213",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1584605565,
+            "last_rendertime": 1584691902,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
